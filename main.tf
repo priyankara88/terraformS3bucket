0b172ac5b8f44725fc91f36187ae527a9b23686c
Priyankara Senarathne <priyankara.sliit@gmail.com>
2025-04-03 09:37:33 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name"  
+  acl    = "private"                
+}
